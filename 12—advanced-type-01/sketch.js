ed93a1c577a0fc0e4028032bacfb7fb60be14c49
alisq <ali.shamas.qadeer@gmail.com>
2022-03-09 15:23:45 -0500

    Update sketch.js

--- 12—advanced-type-01/sketch.js
+++ 12—advanced-type-01/sketch.js
@@ -1,7 +1,9 @@
 var ourFont; //create an empty global variable.
 
+//a reminder: in order to load external assets, you need to have a localserver running.
 function preload() {
-		ourFont = loadFont("assets/SpaceMono-Bold.ttf")
+	//this loads the font data into a global variable	
+	ourFont = loadFont("assets/SpaceMono-Bold.ttf")
 }
 
 var points;
@@ -9,15 +11,19 @@ var points;
 function setup() {
 	createCanvas(1000,1000)
 	textSize(500)
+	
+	//calling our font variable before loading the text
 	textFont(ourFont)
 	// text("hello",100,800)
 
 	fill(0)
 	noStroke()
 
+						//textToPoints doesn't draw the text on your canvas, instead it generates the data of your shape
+						//here's a video tutorial of it: https://www.youtube.com/watch?v=wbDF6xcgvV8&t=725s&ab_channel=xinxin
 
-	points = ourFont.textToPoints("hello",100,800,500);
-	console.log(points)
+	points = ourFont.textToPoints("h",100,800,900);
+	
 
 }
 
@@ -28,7 +34,9 @@ function draw() {
 	for(var i=0;i<points.length;i++) {
 		
 		var circleRad = random(2,10)
-		circle(points[i].x, points[i].y,circleRad)
+		var xPos = points[i].x
+		var yPos = points[i].y
+		circle(xPos, yPos,circleRad)
 	}
 	
 }
